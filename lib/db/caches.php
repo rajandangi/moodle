ab16bbc93872dddee476495c157373aa2e287726
rajandangi <rajandangi@catalyst-au.net>
2024-08-16 16:34:42 +0930

    MDL-79628 navigation: Refactor navigation_cache to use MUC

--- lib/db/caches.php
+++ lib/db/caches.php
@@ -618,4 +618,12 @@ $definitions = array(
         'simpledata' => true,
         'staticacceleration' => true,
     ],
+
+    // The navigation_cache class used this cache to store the navigation nodes.
+    'navigation_cache' => [
+        'mode' => cache_store::MODE_SESSION,
+        'simplekeys' => true,
+        'simpledata' => true,
+        'ttl' => 1800,
+    ],
 );
